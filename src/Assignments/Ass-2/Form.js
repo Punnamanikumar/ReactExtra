392e87219ce39f7a2ea649853acad6b3e3ed46a2
Punnamanikumar <punnamanikumar@gmail.com>
2022-02-14 19:28:22 +0530

    Assignment-2

--- src/Assignments/Ass-2/Form.js
+++ src/Assignments/Ass-2/Form.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 class Form extends React.Component {
   constructor() {
     super();
@@ -9,18 +10,18 @@ class Form extends React.Component {
       item: [],
     };
   }
+
   handelInput = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
+
   handelSubmit = (e) => {
     e.preventDefault();
-    if (
-      this.state.name === "" ||
-      this.state.department === "" ||
-      this.state.rating === ""
-    ) {
+
+    if( this.state.name === "" || this.state.department === "" || this.state.rating === "" ){
       alert("Please Fill all the data");
-    } else {
+    } 
+    else {
       const tempArr = this.state.item;
       const tempObj = {
         name: this.state.name,
@@ -33,45 +34,24 @@ class Form extends React.Component {
       });
     }
   };
+  
   render() {
     return (
-      <>
-        <h1>EMPLOYEE FEEDBACK FORM</h1>
+      <><h1>EMPLOYEE FEEDBACK FORM</h1>
         <form>
           <label>Name</label>
-          <input
-            name="name"
-            onChange={this.handelInput}
-            value={this.state.name}
-            placeholder="Enter your name"
-            className="input3"
-          />
-          <br />
+          <input name="name" onChange={this.handelInput} value={this.state.name} placeholder="Enter your name" className="input3" /><br />
           <label>Department</label>
-          <input
-            name="department"
-            onChange={this.handelInput}
-            value={this.state.department}
-            placeholder="Enter your department"
-          />
-          <br />
+          <input name="department" onChange={this.handelInput} value={this.state.department} placeholder="Enter your department"/><br />
           <label>Rating</label>
-          <input
-            name="rating"
-            onChange={this.handelInput}
-            value={this.state.rating}
-            placeholder="Rating"
-            className="input3"
-          />
-          <br />
+          <input name="rating" onChange={this.handelInput} value={this.state.rating} placeholder="Rating" className="input3"/><br />
           <button onClick={this.handelSubmit}>Submit</button>
         </form>
         <div className="Parentdiv">
           {this.state.item.map((val, index) => {
             return (
               <div key={this.state.name} className="Childdiv">
-                Name: {val.name} || Department :{val.department} || Rating:
-                {val.rating}
+                Name: {val.name} || Department :{val.department} || Rating:{val.rating}
               </div>
             );
           })}
